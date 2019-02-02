011c058ebabd4d4b84887f933d5629417e746f37
Nuno Alexandre <nunombalexandre@gmail.com>
2019-02-02 20:11:14 +0100

    Add Stork workspace & project

---
+++ Stork/Stork/Stork.h
@@ -0,0 +1,19 @@
+//
+//  Stork.h
+//  Stork
+//
+//  Created by Nuno Alexandre on 02/02/2019.
+//  Copyright © 2019 Nuno Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stork.
+FOUNDATION_EXPORT double StorkVersionNumber;
+
+//! Project version string for Stork.
+FOUNDATION_EXPORT const unsigned char StorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stork/PublicHeader.h>
+
+
