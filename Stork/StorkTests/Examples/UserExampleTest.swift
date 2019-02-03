8c52d06a6079eb146b155ab61d987d93835923ed
Nuno Alexandre <nunombalexandre@gmail.com>
2019-02-03 22:51:40 +0100

    Add new unit-tested example 'UserExampleTest'

--- Stork/StorkTests/Examples/UserExampleTest.swift
+++ Stork/StorkTests/Examples/UserExampleTest.swift
@@ -2,6 +2,42 @@ import XCTest
 
 class UserExampleTest: XCTestCase {
 
+  func testUserFromJson() {
+    let user: JSON =
+      [ "id": 123
+      , "name": "Nuno"
+      , "subscription": "free"
+      , "favouriteSongs":
+        [
+          [ "name": "Freedom Is A Voice"
+          , "band": "Bobby Mcferrin"
+          ]
+        , [ "name": "On Tour"
+          , "band": "Kurt Vile"
+          ]
+        , [ "name": "Joana Transmontana"
+          , "band": "B Fachada"
+          ]
+        ]
+      ]
+
+    if let user = User.from(json: user) {
+      XCTAssertEqual(user.id, 123)
+      XCTAssertEqual(user.name, "Nuno")
+      XCTAssertEqual(user.email, nil)
+      XCTAssertEqual(user.subscription, Subscription.free)
+      XCTAssertEqual(
+        user.favouriteSongs,
+        [ Song(name: "Freedom Is A Voice", band: "Bobby Mcferrin")
+        , Song(name: "On Tour", band: "Kurt Vile")
+        , Song(name: "Joana Transmontana", band: "B Fachada")
+        ]
+      )
+    }
+    else {
+      XCTFail("The JSON object failed to be decoded to 'User'")
+    }
+  }
 }
 
 
@@ -22,12 +58,10 @@ enum Subscription: String {
   case gold
 }
 
-struct Song {
+struct Song: Equatable {
   let name: String
   let band: String
 }
-
-
 
 
 /// Comply to the 'FromJson' protocol
