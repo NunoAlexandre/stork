f1f0e518af69150d0e4f7613921d2f917b8dde24
Jae Perris <jae@kidiyo.com>
2019-05-07 15:41:38 +0200

    Add TODO regarding a need to replace Bundle
    This is due to Bundle(for: ) as it is not implemented on the Swift
    open source project as of 7/5/2019. This can be seen by running
    `swift test` from inside the ./Stork directory. Producing this output:
    
    ```
    Fatal error: init(for:) is not yet implemented
    ```

--- Stork/Tests/StorkTests/Extensions/XCTest.swift
+++ Stork/Tests/StorkTests/Extensions/XCTest.swift
@@ -2,6 +2,10 @@ import XCTest
 
 @testable import Stork
 
+/* TODO:(nuno) implement replacement for "Bundle" to import types.json
+*  Bundle(for: ) is not implemented on the Swift open source project 7/5/2019
+*/
+
 extension XCTestCase {
   func getJSON(fromFile file: String) -> Any {
     let inventoryFileURL = Bundle(for: type(of: self)).url(
