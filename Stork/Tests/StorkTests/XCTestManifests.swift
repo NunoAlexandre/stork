75ca8191eaf36accfed33d6db92bb77488c00e5e
Jae Perris <jae@kidiyo.com>
2019-05-07 15:53:50 +0200

    Add TODO for XCTestManifest to be updated
    This is needed as FromJsonItegrationTests are not currently being run.
    They constitute the majority of useful testing to ensure a JSON file can
    be programtically loaded and parsed successfully.

--- Stork/Tests/StorkTests/XCTestManifests.swift
+++ Stork/Tests/StorkTests/XCTestManifests.swift
@@ -4,6 +4,9 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
+    /* TODO:(nuno) implement replacement for "Bundle" to import types.json
+    *  Then implement FromJsonItegrationTests with: testCase(FromJsonItegrationTests.allTests),
+    */
     return [
         testCase(FilmExampleTest.allTests),
         testCase(UserExampleTest.allTests),
